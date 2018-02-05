874b429970805b8da15ec6e3f97a191c0f0f5e22
Zoran Shefot Bogoevski <zbogoevski@gmail.com>
2018-02-05 23:18:07 +0100

    Update conf.php

--- admin/conf/conf.php
+++ admin/conf/conf.php
@@ -1,8 +1,7 @@
 <?php
 
 // Create connection
-$con = mysqli_connect("localhost", "albovita_admin", "simeon08", "albovita_sajt");
-// za kirilica od baza
+$con = mysqli_connect("localhost", "user", "pass", "database");// za kirilica od baza
 mysqli_query($con, "SET NAMES UTF8");
 // Check connection
 if (mysqli_connect_errno($con)) {
